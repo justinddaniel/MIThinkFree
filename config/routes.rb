34a33dfe04a5f55f40cac9e16b37cf9954f9b64c
Justin Daniel <justinddaniel@gmail.com>
2018-11-10 16:27:52 -0500

    Users now have a show page and can go to it from the welcome page when logged in

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     resources :comments, only: [:new, :create, :edit, :update, :destroy]
   end
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
