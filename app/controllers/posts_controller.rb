a18b5bf1b01aeda7d913c0aaeac416a6d2923e0d
Justin Daniel <justinddaniel@gmail.com>
2018-11-17 12:12:26 -0500

    Users can now delete their own posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,4 +26,10 @@ class PostsController < ApplicationController
     @comment = Comment.new
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
+  end
+
 end
