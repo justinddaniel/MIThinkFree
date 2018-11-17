0a1e13345d762a68d799e571d3613d56ff568857
Justin Daniel <justinddaniel@gmail.com>
2018-11-17 16:55:19 -0500

    There is now an index of user blog pages, allowing members to see other member blogs.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,14 @@
 class UsersController < ApplicationController
-  before_action :set_user
+
+  def index
+    @users = User.all
+  end
 
   def show
+    @user = User.find(params[:id])
     @user.profile ? @profile = @user.profile : @profile = Profile.new
     @blogposts = @user.blogposts
     @blogpost = Blogpost.new
   end
 
-  private
-
-    def set_user
-      @user = User.find(params[:id]) || @user = current_user
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :comments, only: [:new, :create, :edit, :update, :destroy]
   end
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  resources :users, only: [:show] do
+  resources :users, only: [:index, :show] do
     resources :profiles, only: [:new, :create, :edit, :update]
     resources :blogposts, only: [:new, :create, :edit, :update, :destroy]
   end
